5232aaf5663686e737915ad7846d485ccb50eb6b
Shihai <Kirill2004val@gmail.com>
2024-07-12 12:44:04 +0300

    feat(our-projects): adaptive

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ function initSliders(){
     const ourProjectsSlider = new Swiper('#our-projects-slider', {
         direction: 'horizontal',
         loop: true,
-        slidesPerView:3,
         spaceBetween: 10,
+
+        breakpoints:{
+            0:{
+                slidesPerView:1
+            },
+
+            620:{
+                slidesPerView:2
+            },
+
+            1024:{
+                slidesPerView:3
+            }
+        },
 
         pagination: {
             el: "#our-projects-slider-dots",
