9bc4a56e1d8b62563e4f490200de5d9465339494
Shihai <Kirill2004val@gmail.com>
2024-07-12 12:30:39 +0300

    feat(our-projects): created

--- script.js
+++ script.js
@@ -4,6 +4,29 @@ function toggleMenu(){
     });
 }
 
+function initSliders(){
+    const ourProjectsSlider = new Swiper('#our-projects-slider', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView:3,
+        spaceBetween: 10,
+
+        pagination: {
+            el: "#our-projects-slider-dots",
+            clickable: true,
+            renderBullet: function (index, className) {
+              return '<span class="' + className + '"></span>';
+            },
+          },
+
+        navigation: {
+            nextEl: "#our-projects-slider__button-next",
+            prevEl: "#our-projects-slider__button-prev",
+          },
+      });
+}
+
 $(document).ready(function(){
     toggleMenu();
+    initSliders();
 });
