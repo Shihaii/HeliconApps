90fe516a34657c168fdacaa23c24c3821821cf9b
Shihai <Kirill2004val@gmail.com>
2024-07-16 18:06:43 +0300

    feat(pricing): created & adaptive

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var form_payment = false;
+var form_type = false;
+
 function toggleMenu() {
   $(document).on("click", "#header__toggle", function () {
     $("header, #header, #header-menu, #header__button").toggleClass("active");
@@ -76,7 +79,22 @@ function initSliders() {
   });
 }
 
+function formSettings(){
+  $("#form-button").attr("disabled","disabled");
+
+  $(document).on("click", "input:radio[name='payment']", function () {
+    form_payment = true;
+    if (form_type == true) $("#form-button").removeClass("disabled").removeAttr("disabled");
+  });
+
+  $(document).on("click", "input:radio[name='type']", function () {
+    form_type = true;
+    if (form_payment == true) $("#form-button").removeClass("disabled").removeAttr("disabled");
+  });
+}
+
 $(document).ready(function () {
   toggleMenu();
   initSliders();
+  formSettings();
 });
